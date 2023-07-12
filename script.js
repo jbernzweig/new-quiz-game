b7aca0a5cfc0af201c1a338edeaeb182545dc24e
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-07-11 20:46:52 -0400

    Answers show up now

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 let body = document.body;
 let h1El = document.createElement("h1");
 let quizBox = document.createElement("div");
-let timerBox = document.createElement("div")
+let timerBox = document.createElement("div");
+let questionIndex=0; 
+let currentAnswer, currentQuestion;
+
 
 quizBox.setAttribute("id", "quizBox");
 questionBox = document.createElement("div");
 questionBox.setAttribute("id", "questionBox");
-let answers = document.createElement("div");
+let answerBox = document.createElement("div");
 
 const startButton = document.createElement("button");
 startButton.innerHTML = "Start";
 
 h1El.textContent = "Welcome to the Coding Quiz";
-answers.textContent = "Answers";
-questionBox.textContent = qList[0].question;
 quizBox.appendChild(timerBox);
 quizBox.appendChild(h1El);
 
 body.appendChild(quizBox);
 quizBox.appendChild(questionBox);
 
-quizBox.appendChild(answers);
+quizBox.appendChild(answerBox);
 quizBox.appendChild(startButton)
 
 h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
-answers.setAttribute("style", "font-size:25px; text-align:center;");
+// answerBox.setAttribute("style", "font-size:25px; text-align:center;");
 
 let time = qList.length*2;
 let timer;
@@ -42,7 +43,29 @@ const startQuiz = () => {
     timer = setInterval(clockTick, 1000);
     timerBox.textContent=time;
     console.log(qList)
+    getQuestions();
     startButton.setAttribute("class", "hide");
+
+}
+
+const showChoices = (choices) => {
+    let writeChoice
+    choices.forEach((choice, index) => {
+        let newDiv = document.createElement("div")
+        quizBox.appendChild(newDiv)
+        newDiv.textContent = choice
+    }) 
+}
+
+const getQuestions = () => {
+    currentQuestion=qList[questionIndex]
+    questionBox.textContent = currentQuestion.question;
+    answerBox.textContent = showChoices(currentQuestion.choices)
+}
+
+const showAnswer = () => {
+    answerBox.textContent = "Answers";
+
 }
 
 const endQuiz = () => {
